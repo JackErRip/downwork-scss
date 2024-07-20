16e8aab6034635e59b39fcc20890e8be0c3cf9d7
John Price <136085120+JackErRip@users.noreply.github.com>
2024-07-20 23:25:02 +0800

    Just added Section 5 and also fixed some minor bugs and tweaks

--- src/components/sections/Section5/Section5.js
+++ src/components/sections/Section5/Section5.js
@@ -6,29 +6,30 @@ function Section5() {
   return (
     <div className={styles.sectionFive}>
       <div className={styles.container}>
-        <h1>
+        <h1 className={styles.title}>
           Get ahead with AI
         </h1>
-        <h2>
+        <h2 className={styles.subtitle}>
           Partner with independent talent on Upwork to help AI work for your business
         </h2>
-        <div>
-          <p>
+        <YouTubeEmbeded classname="s5yemobile" ytLink="https://www.youtube.com/embed/GtL1huin9EE" />
+        <div className={styles.subtext}>
+          <p className={styles.ptextspace}>
             Build a custom chatbot
           </p>
-          <p>
+          <p className={styles.ptextspace}>
             Label your training data
           </p>
-          <p>
+          <p className={styles.ptextspace}>
             Develop your own AI model
           </p>
-          <p>
+          <p className={styles.ptextspace}>
             And more with our Expert-Vetted Talent
           </p>
         </div>
       </div>
       <div>
-        <YouTubeEmbeded ytLink="https://www.youtube.com/embed/GtL1huin9EE" />
+        <YouTubeEmbeded classname="s5ye" ytLink="https://www.youtube.com/embed/GtL1huin9EE" />
       </div>
     </div>
   );
